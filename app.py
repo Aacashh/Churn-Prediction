53894f06a92e31a827173062ea93f3b820d6a472
Aakash Sharma <96709665+Aacashh@users.noreply.github.com>
2023-09-06 20:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ gen_enc = np.load('gen_enc.npy', allow_pickle=True)
 # gen_enc.classes_ = np.load('gen_enc.npy', allow_pickle=True).item()
 loc_enc = np.load('loc_enc.npy', allow_pickle=True)
 # loc_enc.classes_ = np.load('loc_enc.npy', allow_pickle=True).item()
-encoded_gender = gen_enc.transform([gender])[0]
-encoded_location = loc_enc.transform([location])[0]
+encoded_gender = gen_enc.transform([gender][0])
+encoded_location = loc_enc.transform([location][0])
 
 bill_to_usage_ratio = monthly_bill / total_usage if total_usage != 0 else 0
 age_x_subscription_length = age * subscription_length
